57fe3611248a9296732d3e229afed8bb71d34427
abhinav2188 <abhinav.mca19.du@gmail.com>
2020-04-30 00:07:54 +0530

    simple longitute, latitude fetching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,75 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Location from "./components/location";
+import earth from "./svgs/worldwide (1).svg";
+var intervalID;
 
 function App() {
-  return (
-    <div className="App">
-    <h2>Location Trail</h2>
+  const [coordinates,setCoordinates] = useState([]);
+  const[msg,setMsg] = useState("");
+  const[locationEnabled, setLocationEnabled] = useState(false);
+ 
+  function getLocation(position) {
+    console.log("location");
+    
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition( position => {
+        setLocationEnabled(true);
+        setCoordinates( prevState => ([
+          ...prevState, {
+          latitude : position.coords.latitude,
+          longitude : position.coords.longitude,
+          timeStamp : new Date(position.timestamp).toLocaleString()
+        }]));  
+      } , error => {
+        setLocationEnabled(false);
+        if(error.PERMISSION_DENIED){
+          setMsg("location permission denied");
+        }else if(error.POSITION_UNAVAILABLE){
+          setMsg("location unavailable");
+        }else if(error.TIMEOUT){
+          setMsg("location request timeout");
+        }else{
+          setMsg(error.message);
+        }
+      })
+    }else{
+      setMsg("Geolocation is not supported by this browser.");
+    }
+    }
+  
+  function startLocationTrail(){
+    getLocation();
+    intervalID = setInterval(() => {
+      getLocation(); 
+      console.log("loc")
+    },10000);
+  }
+  function stopLocationTrail(){
+    setLocationEnabled(false);
+    console.log(intervalID);
+    clearInterval(intervalID);
+  }
+
+   return (
+    <div className="p-4 ">
+      <img className={`w-6 ml-auto ${locationEnabled ? "opacity-100" : "opacity-25"}`} src={earth} alt=""/>
+      <div className="md:w-1/2 mx-auto">
+        <h2 className="text-lg text-center font-bold my-4 px-4">
+          <p className="my-2">A simple location trailing app to get your current location trail. Click on the
+          <button className="bg-green-400 rounded shadow-sm mx-1 font-bold px-1" onClick={startLocationTrail}>start</button> 
+          button and enable geolocation to start your location trail.</p>
+          <p className="my-2">to stop location trail click
+          <button onClick={stopLocationTrail} className={`px-1 rounded shadow-sm mx-1 font-bold ${locationEnabled ? " bg-red-500" : "bg-gray-400"}`} disabled={!locationEnabled}>stop</button>
+          </p>
+        </h2>
+        <div className="md:p-8 p-4">
+          {coordinates.reverse(-1).map( (coordinate,index) => (
+            <Location key={index} latitude={coordinate.latitude} longitude={coordinate.longitude} timeStamp={coordinate.timeStamp} />
+          ))}
+          <p>{msg}</p>
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/location.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const location = (props) => {
+    return (
+        <div className="border-l border-blue-400 h-16 rounded-sm flex flex-col  relative">
+            <svg className="w-10 absolute top-0 left-0 transform -translate-x-1/2 -translate-y-1/2" viewBox="0 0 100 100">
+                <circle cx="50" cy="50" r="25" fill="blue" />
+                <circle cx="50" cy="50" r="40" fill="blue" opacity="0.5">
+                <animate attributeType="CSS" attributeName="r" 
+                    from="40" to="30" dur="1s" repeatCount="indefinite"/>
+                </circle>
+            </svg>
+            {/* <span className="text-sm self-end mb-1">{props.timeStamp}</span> */}
+            <span className="ml-4 pl-4 ">Longitude : {props.longitude} Latitude : {props.latitude}</span>
+        </div>
+    );
+}
+
+export default location;
